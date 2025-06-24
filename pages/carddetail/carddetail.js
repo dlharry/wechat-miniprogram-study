f54f2e529ac45f8e1bfe6c35d9686442e3180b6e
Hanmin Liu <dlharry@126.com>
2025-06-25 04:46:02 +0800

    add onClick

--- pages/carddetail/carddetail.js
+++ pages/carddetail/carddetail.js
@@ -1,4 +1,4 @@
-// pages/index/index.js
+// pages/carddetail/carddetail.js
 Page({
 
   /**
@@ -62,11 +62,5 @@ Page({
    */
   onShareAppMessage() {
 
-  },
-
-  onClick:function() {
-    wx.navigateTo({
-      url: '/pages/carddetail/carddetail',
-    })
   }
 })
